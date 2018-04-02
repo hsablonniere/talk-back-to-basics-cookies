5b1f5bd39076dfad26943871fd6b4bdc6d592bb7
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2018-04-03 01:02:36 +0200

    Improve dialogue bubbles

--- src/templates/dialogue.js
+++ src/templates/dialogue.js
@@ -6,6 +6,6 @@ module.exports = (node) => {
   const attrs = node.getAttributes();
   const attribution = attrs.attribution.toLowerCase();
   return slide('dialogue', node, `<div class="sentenceWrapper" data-character="${attribution}">
-  <div class="sentence">${node.getContent()}</div>
+  <div class="sentence">${node.getContent().replace('  ', '<br>')}</div>
 </div>`);
 };
