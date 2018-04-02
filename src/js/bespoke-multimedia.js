1c66554dd14ca0e2c936b9a180d84129098bda46
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2018-04-03 01:04:40 +0200

    Remember init time position on videos

--- src/js/bespoke-multimedia.js
+++ src/js/bespoke-multimedia.js
@@ -2,18 +2,27 @@
 
 export default function configurePlugin () {
 
+  const startTimeCache = new WeakMap();
+
   return function initPlugin (deck) {
 
     deck.on('activate', ({ slide }) => {
       Array.from(slide.querySelectorAll('video'))
-        .forEach((video) => video.play());
+        .forEach((video) => {
+          const startTime = startTimeCache.get(video);
+          if (!startTime) {
+            startTimeCache.set(video, video.currentTime);
+          }
+          return video.play();
+        });
     });
 
     deck.on('deactivate', ({ slide }) => {
       Array.from(slide.querySelectorAll('video'))
         .forEach((video) => {
           video.pause();
-          video.currentTime = 0;
+          const startTime = startTimeCache.get(video);
+          video.currentTime = startTime;
         });
     });
   };
